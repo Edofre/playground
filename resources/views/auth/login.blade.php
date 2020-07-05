6b543e91d3caca37cbcc0b16c1a0b33d26d2b610
Edo Freriks <edofre@gmail.com>
2020-07-05 14:41:41 +0200

    Login styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
 <div class="container mx-auto">
     <div class="flex flex-wrap justify-center">
         <div class="w-full max-w-sm">
-            <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
+            <div class="flex flex-col break-words bg-white border rounded shadow-md">
 
                 <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
                     {{ __('auth.login') }}
@@ -21,7 +21,7 @@
                         </label>
                         <input id="email" type="email" class="form-input w-full @error('email') border-red-500 @enderror" placeholder="{{ __('auth.email') }}" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                         @error('email')
-                        <p class="text-red-500 text-xs italic mt-4">
+                        <p class="text-red-500 text-xs italic mt-1">
                             {{ $message }}
                         </p>
                         @enderror
@@ -31,9 +31,9 @@
                         <label for="password" class="block text-gray-700 text-sm font-bold mb-2 sr-only">
                             {{ __('auth.password') }}
                         </label>
-                        <input id="password" type="password" class="form-input w-full @error('password') border-red-500 @enderror"  placeholder="{{ __('auth.password') }}" name="password" required>
+                        <input id="password" type="password" class="form-input w-full @error('password') border-red-500 @enderror" placeholder="{{ __('auth.password') }}" name="password" required>
                         @error('password')
-                        <p class="text-red-500 text-xs italic mt-4">
+                        <p class="text-red-500 text-xs italic mt-1">
                             {{ $message }}
                         </p>
                         @enderror
@@ -45,28 +45,24 @@
                             <span class="ml-2">{{ __('auth.remember_me') }}</span>
                         </label>
 
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-auto">
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-100 py-1 px-3 rounded focus:outline-none focus:shadow-outline ml-auto">
                             {{ __('Login') }}
                         </button>
                     </div>
-
-                    <div class="flex flex-wrap items-center">
-                        @if (Route::has('password.request'))
-                        <a class="text-sm text-blue-500 hover:text-blue-700 whitespace-no-wrap no-underline" href="{{ route('password.request') }}">
-                            {{ __('auth.forgot_your_password') }}
-                        </a>
-                        @endif
-                    </div>
                 </form>
 
-                @if (Route::has('register'))
-                <div class="bg-gray-200 text-xs text-center italic text-gray-700 py-3">
-                    {{ __('auth.dont_have_an_account') }}
-                    <a class="text-blue-500 hover:text-blue-700 no-underline" href="{{ route('register') }}">
-                        {{ __('auth.register') }}
-                    </a>
+                <div class="bg-gray-200 text-xs italic text-gray-700 py-3 px-6 flex">
+                    @if (Route::has('password.request'))
+                        <a class="text-blue-500 hover:text-blue-700 mr-auto" href="{{ route('password.request') }}">
+                            {{ __('auth.forgot_your_password') }}
+                        </a>
+                    @endif
+                    @if (Route::has('register'))
+                        <a class="text-blue-500 hover:text-blue-700 ml-auto" href="{{ route('register') }}">
+                            {{ __('auth.register') }}
+                        </a>
+                    @endif
                 </div>
-                @endif
             </div>
         </div>
     </div>
