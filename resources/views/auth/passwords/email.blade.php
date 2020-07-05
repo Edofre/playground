ea6b13795d20088e33f29d82fd542949f18c1b78
Edo Freriks <edofre@gmail.com>
2020-07-05 16:35:19 +0200

    Reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,7 +4,6 @@
 
 @section('content')
 
-
 <div class="container mx-auto">
     <div class="flex flex-wrap justify-center">
         <div class="w-full max-w-sm">
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -13,28 +13,51 @@
                     {{ __('auth.reset_password') }}
                 </div>
 
-                <form method="POST" action="{{ route('password.update') }}">
+                <form class="w-full p-6" method="POST" action="{{ route('password.update') }}">
                     @csrf
                     <input type="hidden" name="token" value="{{ $token }}">
 
-                    <label for="email">{{ __('auth.email') }}</label>
-                    <input id="email" type="email" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-                    @error('email')
-                    <strong>{{ $message }}</strong>
-                    @enderror
+                    <div class="flex flex-wrap mb-3">
+                        <label for="email" class="block text-gray-700 text-sm font-bold mb-2 sr-only">
+                            {{ __('auth.email') }}
+                        </label>
+                        <input id="email" type="email" class="form-input w-full @error('email') border-red-500 @enderror" placeholder="{{ __('auth.email') }}" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        @error('email')
+                        <p class="text-red-500 text-xs italic mt-1">
+                            {{ $message }}
+                        </p>
+                        @enderror
+                    </div>
 
-                    <label for="password">{{ __('auth.password') }}</label>
-                    <input id="password" type="password" name="password" required autocomplete="new-password">
-                    @error('password')
-                    <strong>{{ $message }}</strong>
-                    @enderror
+                    <div class="flex flex-wrap mb-3">
+                        <label for="password" class="block text-gray-700 text-sm font-bold mb-2 sr-only">
+                            {{ __('auth.password') }}
+                        </label>
+                        <input id="password" type="password" class="form-input w-full @error('password') border-red-500 @enderror" placeholder="{{ __('auth.password') }}" name="password" required autocomplete="new-password">
+                        @error('password')
+                        <p class="text-red-500 text-xs italic mt-1">
+                            {{ $message }}
+                        </p>
+                        @enderror
+                    </div>
 
-                    <label for="password-confirm">{{ __('auth.confirm_password') }}</label>
-                    <input id="password-confirm" type="password" name="password_confirmation" required autocomplete="new-password">
+                    <div class="flex flex-wrap mb-3">
+                        <label for="password_confirmation" class="block text-gray-700 text-sm font-bold mb-2 sr-only">
+                            {{ __('auth.password_confirmation') }}
+                        </label>
+                        <input id="password_confirmation" type="password" class="form-input w-full @error('password_confirmation') border-red-500 @enderror" placeholder="{{ __('auth.password_confirmation') }}" name="password_confirmation" required autocomplete="new-password">
+                        @error('password_confirmation')
+                        <p class="text-red-500 text-xs italic mt-1">
+                            {{ $message }}
+                        </p>
+                        @enderror
+                    </div>
 
-                    <button type="submit">
-                        {{ __('auth.reset_password') }}
-                    </button>
+                    <div class="flex">
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-100 py-1 px-3 rounded focus:outline-none focus:shadow-outline ml-auto">
+                            {{ __('auth.reset_password') }}
+                        </button>
+                    </div>
                 </form>
 
             </div>
