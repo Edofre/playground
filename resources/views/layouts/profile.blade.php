7e86c05b4ecb99c7dc164c99aecd0127a9243d59
Edo Freriks <edofre@gmail.com>
2020-08-16 18:33:46 +0200

    profile updates

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -13,14 +13,12 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body class=bg-gray-100>
+<body class=bg-gray-200>
     <header class="bg-white shadow">
         @yield('header')
     </header>
     <main>
-        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            @yield('content')
-        </div>
+        @yield('content')
     </main>
 </body>
 
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -3,19 +3,21 @@
 @section('title', 'Edo Freriks')
 
 @section('content')
-<div class="px-4 py-6">
-    <div class="bg-white border-gray-200 rounded-lg p-2">
 
-        <img class="h-24 w-24 object-cover rounded-full mx-auto" src="{{ asset('img/edo.png') }}">
+<div class="md:w-1/2 mx-auto px-4 py-6">
+    <div class="md:flex bg-white rounded-lg p-6">
+        <img class="h-24 w-24 rounded-full mx-auto md:mx-0 md:mr-6 object-cover" src="{{ asset('img/edo.png') }}">
+        <div class="text-center md:text-left">
+            <h2 class="text-xl text-gray-700">Edo Freriks</h2>
+            <div class="text-green-500">Software developer</div>
 
-        <h1>Edo Freriks</h1>
+            <div class="text-gray-700 sm:mt-0 md:mt-4">
+                <a href="https://github.com/edofre"><i class="fab fa-github hover:text-green-500"></i></a>
+                <a href="https://www.linkedin.com/in/edo-freriks/"><i class="fab fa-linkedin-in hover:text-green-500"></i></a>
+                <a href="https://twitter.com/edofre"><i class="fab fa-twitter hover:text-green-500"></i></a>
 
-        <div >
-            <i class="fab fa-github"></i>
-            <i class="fab fa-linkedin-in"></i>
-            <i class="fab fa-twitter"></i>
-
-            <i class="fas fa-at"></i>
+                <a href="mailto:edofre@gmail.com"><i class="fas fa-at hover:text-green-500"></i></a>
+            </div>
         </div>
     </div>
 </div>
