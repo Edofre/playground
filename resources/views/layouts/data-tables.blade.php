bd1a897862c01e2c5f855e3fd053841ce275b13a
Edo Freriks <edofre@gmail.com>
2020-09-05 22:21:25 +0200

    Start of datatables styling

--- resources/views/layouts/data-tables.blade.php
+++ resources/views/layouts/data-tables.blade.php
@@ -17,7 +17,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 
     {{-- DataTables --}}
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
+    {{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css"> --}}
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
 
     {{-- DataTables Response --}}
